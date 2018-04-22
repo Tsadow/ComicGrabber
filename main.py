49ab3f27360f60c655c31882e4a9798ebe1a88b8
Tom Castle <minifigone@gmail.com>
2018-04-22 17:28:19 -0400

    successfully getting something from a website

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 
 # imports
-
-#
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+import http.client
 
 def main():
-    var = 0
+    links = {"xkcd.com"}
+
+    for link in links:
+        conn = http.client.HTTPSConnection(link)
+        conn.request("GET", "/")
+        doc = conn.getresponse()
+        soup = BeautifulSoup(doc, "html.parser")
+        print(soup.prettify())
 
 # run main
 main()

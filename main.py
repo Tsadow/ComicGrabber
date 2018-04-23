209c04338e935fd782fce4b2514426bed399c096
Tom Castle <minifigone@gmail.com>
2018-04-23 19:36:07 -0400

    abstracted out get_document_object() hostname issues with don't hit save ssl cert verification issues with chuck and gwtb

--- main.py
+++ main.py
@@ -8,14 +8,24 @@ import re
 
 def main():
     get_xkcd()
-    get_dhs()
+    #get_dhs()
+    get_mollybeans()
+    #get_callmechuck()
+    #get_gwtb()
 
-def get_xkcd():
-    link = "xkcd.com"
+def get_document_object(link):
     conn = http.client.HTTPSConnection(link)
     conn.request("GET", "/")
     doc = conn.getresponse()
-    soup = BeautifulSoup(doc, "html.parser")
+    return BeautifulSoup(doc, "html.parser")
+
+def get_xkcd():
+    link = "xkcd.com"
+    #conn = http.client.HTTPSConnection(link)
+    #conn.request("GET", "/")
+    #doc = conn.getresponse()
+    #soup = BeautifulSoup(doc, "html.parser")
+    soup = get_document_object(link)
 
     imge = soup.select_one("#comic > img")    
     src_string = imge['src']
@@ -27,10 +37,11 @@ def get_xkcd():
 
 def get_dhs():
     link = "donthitsave.com"
-    conn = http.client.HTTPSConnection(link)
-    conn.request("GET", "/")
-    doc = conn.getresponse()
-    soup = BeautifulSoup(doc, "html.parser")
+    #conn = http.client.HTTPSConnection(link)
+    #conn.request("GET", "/")
+    #doc = conn.getresponse()
+    #soup = BeautifulSoup(doc, "html.parser")
+    soup = get_document_object(link)
 
     imge = soup.select_one(".comicfull")
     src_string = imge['src']
@@ -40,6 +51,40 @@ def get_dhs():
 
     urllib.request.urlretrieve(src_string, local_name)
 
+def get_mollybeans():
+    link = "mollybeans.com"
+    soup = get_document_object(link)
 
+    imge = soup.select_one(".entry-comic > article > a > img")
+    src_string = imge['src']
+
+    extension = ".png"
+    local_name = link + extension
+
+    urllib.request.urlretrieve(src_string, local_name)
+
+def get_callmechuck():
+    link = "callmechuckcomic.com"
+    soup = get_document_object(link)
+
+    imge = soup.select_one(".entry-comic > article > a > img")
+    src_string = imge['src']
+
+    extension = ".png"
+    local_name = link + extension
+
+    urllib.request.urlretrieve(src_string, local_name)
+
+def get_gwtb():
+    link = "www.blastwave-comic.com"
+    soup = get_document_object(link)
+
+    imge = soup.select_one(".comic_ruutu > center > img")
+    src_string = imge['src']
+
+    extension = ".png"
+    local_name = link + extension
+
+    urllib.request.urlretrieve(src_string, local_name)
 # run main
 main()

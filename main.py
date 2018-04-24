e3efbf4860d9a4e68ea7e135864c91f48e1a556c
Tom Castle <minifigone@gmail.com>
2018-04-23 20:18:56 -0400

    made chuck and gwtb work by using http instead of https

--- main.py
+++ main.py
@@ -11,18 +11,24 @@ def main():
     get_xkcd()
     #get_dhs()
     get_mollybeans()
-    #get_callmechuck()
-    #get_gwtb()
+    get_callmechuck()
+    get_gwtb()
 
-def get_document_object(link):
+def get_document_object_https(link):
     conn = http.client.HTTPSConnection(link)
+    conn.request("GET", "/")
+    doc = conn.getresponse()
+    return BeautifulSoup(doc, "html.parser")
+
+def get_document_object_http(link):
+    conn = http.client.HTTPConnection(link)
     conn.request("GET", "/")
     doc = conn.getresponse()
     return BeautifulSoup(doc, "html.parser")
 
 def get_xkcd():
     link = "xkcd.com"
-    soup = get_document_object(link)
+    soup = get_document_object_https(link)
 
     imge = soup.select_one("#comic > img")    
     src_string = imge['src']
@@ -34,7 +40,7 @@ def get_xkcd():
 
 def get_dhs():
     link = "donthitsave.com"
-    soup = get_document_object(link)
+    soup = get_document_object_https(link)
 
     imge = soup.select_one(".comicfull")
     src_string = imge['src']
@@ -46,7 +52,7 @@ def get_dhs():
 
 def get_mollybeans():
     link = "mollybeans.com"
-    soup = get_document_object(link)
+    soup = get_document_object_https(link)
 
     imge = soup.select_one(".entry-comic > article > a > img")
     src_string = imge['src']
@@ -58,7 +64,7 @@ def get_mollybeans():
 
 def get_callmechuck():
     link = "callmechuckcomic.com"
-    soup = get_document_object(link)
+    soup = get_document_object_http(link)
 
     imge = soup.select_one(".entry-comic > article > a > img")
     src_string = imge['src']
@@ -70,10 +76,13 @@ def get_callmechuck():
 
 def get_gwtb():
     link = "www.blastwave-comic.com"
-    soup = get_document_object(link)
+    soup = get_document_object_http(link)
 
-    imge = soup.select_one(".comic_ruutu > center > img")
+    imge = soup.select_one(".comic_title ~ img")
     src_string = imge['src']
+
+    #nonstandard src_string because of how site references the image files
+    src_string = "http://www.blastwave-comic.com" + src_string[1:]
 
     extension = ".png"
     local_name = link + extension
